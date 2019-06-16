1c8a8fd9f2646d9e0fcc737a2ba33d265481b723
ovikosta <ovikosta@gmail.com>
2019-06-17 00:57:10 +0300

    Add function download_site and write_to_file.

--- dwsite.py
+++ dwsite.py
@@ -1,14 +1,38 @@
 #!/usr/bin/python
+#Required python>=3.5
 import sys
 import argparse
+import http.client
+import ssl
+from html.parser import HTMLParser
 
-def createParser():
+def create_parser_arg():
     parser = argparse.ArgumentParser(description='Download site.')
-    parser.add_argument('url', required=True)
-    parser.add_argument('dest_folder', default='./')
+    parser.add_argument('-u', '--url', required=True, nargs='?', type=str)
+    parser.add_argument('-df', '--dest-folder', default='./', nargs='?', type=str)
     return parser
 
+def download_site(url: str) -> None:
+    #verification protocol HTTP or HTTPS
+    try:
+        conn = http.client.HTTPSConnection(url) 
+        conn.request('GET', '/')
+    except ssl.SSLCertVerificationError:
+        conn = http.client.HTTPConnection(url)
+        conn.request('GET', '/')
+    resp = conn.getresponse()
+    raw_data = resp.read()
+    data = raw_data.decode('utf-8')
+
+
+
+def write_file(name: str, data) -> None:
+    with open(name, 'w') as file:
+        file.write(data)
+
+
 if __name__ == '__main__':
-    parser = createParser()
+    parser = create_parser_arg()
     namespace = parser.parse_args()
+
 
